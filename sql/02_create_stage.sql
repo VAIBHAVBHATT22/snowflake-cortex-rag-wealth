763fa6b24d61f0007f06296f64c259bccc5d702e
VAIBHAV BHATT <99778682+VAIBHAVBHATT22@users.noreply.github.com>
2026-06-07 16:10:12 +0530

    Update 02_create_stage.sql

--- sql/02_create_stage.sql
+++ sql/02_create_stage.sql
@@ -1,1 +1,9 @@
-CREATE STAGE PDF_STAGE;
+-- Create stage for PDF documents
+
+USE DATABASE WEALTH_RAG_DB;
+USE SCHEMA RAG;
+
+CREATE OR REPLACE STAGE PDF_STAGE;
+
+-- Verify uploaded files
+LIST @PDF_STAGE;
